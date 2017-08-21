bafb1bc3e2a3f534f746c1e59e196b2992f7783f
TimoRuetten <timo.ruetten@gmail.com>
2017-08-21 15:11:03 +0200

    added some tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,28 @@ import SDK from '../lib/index';
 
 const NearestSDK = new SDK({});
 
+/**
+  Tests
+  ----------
+  ----------
+
+  Init:
+  - should init correctly
+  - should init with missing keys and throw error
+
+  Fetch:
+  - should return promise
+  - should call callback
+  - should throw error because of wrong endpoint
+
+  Queries:
+  - should call query without problem
+  - should call wrong query and throw error
+  - should call query and return correct value
+  - should call query with wrong version and throw error
+  - should call query with wrong keys and throw error
+
+*/
 describe('Test FETCH functionality', () => {
   it('fetch should return a promise', (done) => {
     const myPromise = NearestSDK.nodes.get({});
