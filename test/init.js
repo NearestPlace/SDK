9dd95e80a20fe2257a7fe29ad3cc83e52940eadf
Stephan Georg <s.georg@nearest.place>
2017-09-27 14:03:29 +0200

    Excluded apiKey

--- test/init.js
+++ test/init.js
@@ -7,30 +7,9 @@ import './queries/getNearest';
 import { NearestClient } from '../lib/index';
 
 const expect = chai.expect;
-
+const apiKey = process.env.NEARESTAPIKEY || '';
 const TestAppId = '5fHa6zTDBohz4RrsM';
-/**
-  Tests
-  ----------
-  ----------
 
-  Init:
-  - should init correctly
-  - should init with missing keys and throw error
-
-  Fetch:
-  - should return promise
-  - should call callback
-  - should throw error because of wrong endpoint
-
-  Queries:
-  - should call query without problem
-  - should call wrong query and throw error
-  - should call query and return correct value
-  - should call query with wrong version and throw error
-  - should call query with wrong keys and throw error
-
-*/
 describe('Init the SDK', () => {
   it('The SDK init should throw an error because of missing apiKey', (done) => {
     const createInstance = () => new NearestClient();
@@ -39,7 +18,7 @@ describe('Init the SDK', () => {
   });
   it('The SDK init should work correctly', (done) => {
     const NearestSDK = new NearestClient({
-      apiKey: 'Joq7XlDb1529UHe0',
+      apiKey,
       app: TestAppId,
     });
     done();
